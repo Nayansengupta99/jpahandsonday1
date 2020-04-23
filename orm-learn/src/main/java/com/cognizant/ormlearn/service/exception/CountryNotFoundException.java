b82a32337137b8bf2d5f21d723b03c60cb3bf488
Nayan Sengupta <49792458+Nayansengupta99@users.noreply.github.com>
2020-04-23 10:00:02 +0530

    ORM

---
+++ orm-learn/src/main/java/com/cognizant/ormlearn/service/exception/CountryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cognizant.ormlearn.service.exception;
+
+public class CountryNotFoundException extends Throwable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
